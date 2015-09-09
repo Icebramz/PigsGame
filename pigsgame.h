da523743cfce1ecdafb5eafab7bbb710d1eb484c
Kendra's Old <kkuivenhoven@mail.csuchico.edu>
2015-09-09 12:48:07 -0700

    player class

---
+++ pigsgame.h
@@ -0,0 +1,17 @@
+#ifndef PIGSGAME_H
+#define PIGSGAME_H
+
+
+#include <iostream>
+using namespace std;
+
+class Player
+{
+  public:
+    string roll;
+    string hold;
+    int score;
+    string choice;
+};
+
+#endif
