9fcbb191cd7f080054cbd419c482da1e7ad293b9
Abdullah <aalkalbi@mail.csuchico.edu>
2015-09-09 23:15:41 -0700

    changing private and public of the class

--- pigsgame.h
+++ pigsgame.h
@@ -1,3 +1,5 @@
+
+
 #ifndef PIGSGAME_H
 #define PIGSGAME_H
 
@@ -7,11 +9,14 @@ using namespace std;
 
 class Player
 {
-  public:
+  private:  // class fields should be kept as private, if we need to modify them we can use the get/set methods for the respective fields
     string roll;
     string hold;
     int score;
-    string choice;
+    string choice; 
+	// adding a few functions 
+  public: 
+	  int rollDice(); // function to roll a dice and generate a score 
 };
 
 #endif
