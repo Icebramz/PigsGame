9a0323912af91328c17599375ffe58edf3508ae0
Abdullah <aalkalbi@mail.csuchico.edu>
2015-09-14 14:50:44 -0700

    adding the players function

--- pigsgame.h
+++ pigsgame.h
@@ -1,22 +1,23 @@
-
-
 #ifndef PIGSGAME_H
 #define PIGSGAME_H
 
+	#include <iostream>
+	using namespace std;
 
-#include <iostream>
-using namespace std;
+	class Player
+	{
+	private: // class fields should be kept as private, if we need to modify them we can use the get/set methods for the respective fields
+	    string roll;
+	    string hold;
+	    int score;
+	    string choice;
 
-class Player
-{
-  private:  // class fields should be kept as private, if we need to modify them we can use the get/set methods for the respective fields
-    string roll;
-    string hold;
-    int score;
-    string choice; 
-	// adding a few functions 
-  public: 
-	  int rollDice(); // function to roll a dice and generate a score 
-};
+	// adding a few functions
+	public:
+		int rollDice(); // function to roll dice and generate a score
+	};
+
+	void play(Player p1, Player p2); // function that takes in 2 Player class objects to make the "Pigsgame" run
+	
 
 #endif
