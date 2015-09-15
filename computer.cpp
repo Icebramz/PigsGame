c10728c7e4632733c34bb257fba980e1ad928ed5
icebramz <icebramz@gmail.com>
2015-09-14 17:49:41 -0700

    added functionality to computer_roll() function

--- computer.cpp
+++ computer.cpp
@@ -17,4 +17,13 @@ void Computer::play_game()
 {
  cout << "Computer is now playing!" << endl;
  cout << "Computers score is: " << computer_total_score << endl;
+ cout << "Computers initial roll is: " << computer_roll() << endl;
+}
+
+int Computer::computer_roll()
+{
+ int dice_roll;
+ srand(time(NULL));
+ dice_roll = (rand()%6) + 1;
+ return dice_roll;
 }
